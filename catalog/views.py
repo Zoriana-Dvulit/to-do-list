214c4b45d28cf4002fd81d0f5550302c48865e4b
Zoriana Dvulit <zdvulit@ukr.net>
2023-03-30 13:46:35 +0300

    Created 6 templates (tag_list, tag_form, tag_confirm_delete, task_list, task_form, tskg_confirm_delete) in the 'catalog' directory with their respective content. Updated the view names in 'views.py' to reflect the new template names

--- catalog/views.py
+++ catalog/views.py
@@ -9,26 +9,26 @@ from .models import Task, Tag
 class TaskCreateView(CreateView):
     model = Task
     fields = ["title", "description", "due_date", "completed", "tags"]
-    template_name = "tasks/task_form.html"
+    template_name = "catalog/task_form.html"
     success_url = reverse_lazy('task-list')
 
 
 class TaskListView(ListView):
     model = Task
-    template_name = "tasks/task_list.html"
+    template_name = "catalog/task_list.html"
     context_object_name = 'tasks'
 
 
 class TaskUpdateView(UpdateView):
     model = Task
     fields = ["title", "description", "due_date", "completed", "tags"]
-    template_name = "tasks/task_form.html"
+    template_name = "catalog/task_form.html"
     success_url = reverse_lazy('task-list')
 
 
 class TaskDeleteView(DeleteView):
     model = Task
-    template_name = "tasks/task_confirm_delete.html"
+    template_name = "catalog/task_confirm_delete.html"
     success_url = reverse_lazy("task-list")
 
 
@@ -37,24 +37,24 @@ class TaskDeleteView(DeleteView):
 class TagCreateView(CreateView):
     model = Tag
     fields = ["name"]
-    template_name = "tags/tag_form.html"
+    template_name = "catalog/tag_form.html"
     success_url = reverse_lazy("tag-list")
 
 
 class TagListView(ListView):
     model = Tag
-    template_name = "tags/tag_list.html"
+    template_name = "catalog/tag_list.html"
     context_object_name = "tags"
 
 
 class TagUpdateView(UpdateView):
     model = Tag
     fields = ["name"]
-    template_name = "tags/tag_form.html"
+    template_name = "catalog/tag_form.html"
     success_url = reverse_lazy("tag-list")
 
 
 class TagDeleteView(DeleteView):
     model = Tag
-    template_name = "tags/tag_confirm_delete.html"
+    template_name = "catalog/tag_confirm_delete.html"
     success_url = reverse_lazy("tag-list")
