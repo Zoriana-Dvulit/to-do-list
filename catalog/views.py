bcecf63fe9d7b1f9b4b6291a2664522178112812
Zoriana Dvulit <zdvulit@ukr.net>
2023-03-30 12:36:30 +0300

    Added views for task creation, update, deletion, and tag creation

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from .models import Task, Tag
 
-# Create your views here.
+# Task views
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ["title", "description", "due_date", "completed", "tags"]
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy('task-list')
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "tasks/task_list.html"
+    context_object_name = 'tasks'
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = ["title", "description", "due_date", "completed", "tags"]
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy('task-list')
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = "tasks/task_confirm_delete.html"
+    success_url = reverse_lazy("task-list")
+
+
+# Tag views
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = ["name"]
+    template_name = "tags/tag_form.html"
+    success_url = reverse_lazy("tag-list")
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "tags/tag_list.html"
+    context_object_name = "tags"
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = ["name"]
+    template_name = "tags/tag_form.html"
+    success_url = reverse_lazy("tag-list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    template_name = "tags/tag_confirm_delete.html"
+    success_url = reverse_lazy("tag-list")
