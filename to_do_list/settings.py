fa555cd07b3b3bdcf31b1f5ca83c0dac99033d16
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-12 17:14:26 +0300

    Hiding Django Secret Key, adding briefly description and instruction in README.md

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -11,7 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = """
-    django-insecure-ztf*y@2mhna$r+2s#s0y2b_c814c=$$=s$udg-7y5g!+
-    xb=xvj
-"""
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
