7d690ce36bba27bcae0cc7b88ef1c371a75520b8
Zoriana Dvulit <zdvulit@ukr.net>
2023-04-13 15:44:10 +0300

    Removing .idea from GitHub,checking for an empty line, rewriting to class view

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views import generic
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
-
+from django.views.generic import CreateView
 
 from .models import Task, Tag
 from .forms import TagForm, TaskForm
@@ -48,129 +46,64 @@ class TaskUpdateView(generic.UpdateView):
         return super().form_valid(form)
 
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(generic.DeleteView):
     model = Task
     template_name = "catalog/task_confirm_delete.html"
     success_url = reverse_lazy("catalog:task-list")
 
 
-class TagCreateView(CreateView):
+class TagCreateView(generic.CreateView):
     model = Tag
-    fields = ["name"]
+    form_class = TagForm
     template_name = "catalog/tag_form.html"
     success_url = reverse_lazy("catalog:tag_list")
 
 
-class TagListView(ListView):
+class TagListView(generic.ListView):
     model = Tag
     template_name = "catalog/tag_list.html"
     context_object_name = "tags"
 
 
-class TagUpdateView(UpdateView):
+class TagUpdateView(generic.UpdateView):
     model = Tag
-    fields = ["name"]
+    form_class = TagForm
     template_name = "catalog/tag_form.html"
     success_url = reverse_lazy("catalog:tag-list")
 
 
-class TagDeleteView(DeleteView):
+class TagDeleteView(generic.DeleteView):
     model = Tag
     template_name = "catalog/tag_confirm_delete.html"
     success_url = reverse_lazy("catalog:tag-list")
 
 
-def tag_list(request):
-    tags = Tag.objects.all()
-    return render(request, "catalog/tag_list.html", {"tags": tags})
-
-
-@login_required
-def tag_create(request):
-    if request.method == "POST":
-        form = TagForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("tag-list")
-    else:
-        form = TagForm()
-    return render(request, "catalog/tag_form.html", {"form": form})
-
-
-def tag_update(request, pk):
-    tag = get_object_or_404(Tag, pk=pk)
-    if request.method == "POST":
-        form = TagForm(request.POST, instance=tag)
-        if form.is_valid():
-            form.save()
-            return redirect("catalog:tag-detail", pk=pk)
-    else:
-        form = TagForm(instance=tag)
-    return render(request, "catalog/tag_update.html", {"form": form})
-
-
-@login_required
-def tag_delete(request, pk):
-    tag = get_object_or_404(Tag, pk=pk)
-    if request.method == "POST":
-        tag.delete()
-        messages.success(request, "Tag has been deleted!")
-        return redirect("catalog:tag-list")
-
-
-def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, "catalog/task_list.html", {"tasks": tasks})
-
-
-@login_required
-def task_create(request):
-    form = TaskForm(request.POST or None)
-    if form.is_valid():
-        task = form.save(commit=False)
-        task.user = request.user
+class TaskCompleteView(generic.View):
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=kwargs.get("pk"))
+        task.completed = True
+        task.completed_date = timezone.now()
         task.save()
+        messages.success(request, f"Task '{task.content}' has been marked as complete!")
         return redirect("catalog:task-list")
-    return render(request, "catalog/task_create.html", {"form": form})
 
 
-def task_detail(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    return render(request, "catalog/task_detail.html", {"task": task})
+class TaskCreateByUserView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "catalog/task_form.html"
+    success_url = reverse_lazy("catalog:task-list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
-@login_required
-def task_update(request, pk):
-    task = get_object_or_404(Task, pk=pk, user=request.user)
-    form = TaskForm(request.POST or None, instance=task)
-    if form.is_valid():
-        form.save()
-        return redirect("catalog:task_detail", pk=pk)
-    return render(request, "catalog/task_form.html", {"form": form})
+class TaskUpdateByUserView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "catalog/task_form.html"
+    success_url = reverse_lazy("catalog:task-list")
 
-
-@login_required
-def task_delete(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.method == "POST":
-        task.delete()
-        messages.success(request, "Task has been deleted!")
-        return redirect("catalog:task-list")
-    return render(
-        request,
-        "catalog/task_confirm_delete.html",
-        {"object": task}
-    )
-
-
-@login_required
-def task_complete(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    task.completed = True
-    task.completed_date = timezone.now()
-    task.save()
-    messages.success(
-        request,
-        f"Task '{task.content}' has been marked as complete!"
-    )
-    return redirect("task-list")
+    def get_queryset(self):
+        return super().get
